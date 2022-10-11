1f834f11c50cbe012ef5b10cd1441c448b915d7e
Rafael Rosso Coelho <rafaelkrc@hotmail.com>
2022-10-11 17:07:56 +0100

    implementado teste de excessões
    close #13

--- libpythonpro/tests/test_spam/test_enviador_de_email.py
+++ libpythonpro/tests/test_spam/test_enviador_de_email.py
@@ -1,6 +1,6 @@
 import pytest
 
-from libpythonpro.spam.enviador_de_email import Enviador
+from libpythonpro.spam.enviador_de_email import Enviador, EmailInvalido
 
 
 def test_criar_enviador_de_email():
@@ -9,15 +9,30 @@ def test_criar_enviador_de_email():
 
 
 @pytest.mark.parametrize(
-    'destinatario',
+    'remetente',
     ['rafaelkrc@hotmail.com', 'gestaocasacoelho@gmail.com']
 )
-def test_remetente(destinatario):
+def test_remetente(remetente):
     enviador = Enviador()
     resultado = enviador.enviar(
-        destinatario,
+        remetente,
         'rafaelkrc@gmail.com',
         'Curso Pytools',
         'Teste de enviador de email para testes TDD e Baby test.'
     )
-    assert destinatario in resultado
+    assert remetente in resultado
+
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['', 'rafael']
+)
+def test_remetente_invalido(remetente):
+    enviador = Enviador()
+    with pytest.raises(EmailInvalido):
+        enviador.enviar(
+            remetente,
+            'rafaelkrc@gmail.com',
+            'Curso Pytools',
+            'Teste de enviador de email para testes TDD e Baby test.'
+        )
