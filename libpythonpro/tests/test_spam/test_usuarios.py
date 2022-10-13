92ad33c12a19d4493e110386caf22e842d307b7d
Rafael Rosso Coelho <rafaelkrc@hotmail.com>
2022-10-13 11:58:46 +0100

    Produção de codigo testável

--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -2,13 +2,16 @@ from libpythonpro.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Rafael')
+    usuario = Usuario(nome='Rafael', email='rafaelkrc@hotmail.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuarios(sessao):
-    usuarios = [Usuario(nome='Rafael'), Usuario(nome='Rosso')]
+    usuarios = [
+        Usuario(nome='Rafael', email='rafaelkrc@hotmail.com'),
+        Usuario(nome='Rosso', email='rafaelkrc@hotmail.com')
+    ]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
